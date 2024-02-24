d64a1028be66973d84eb2b8c94c03b4ed5878264
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-02-24 22:38:28 +0200

    Featured Post Design.

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Pedalând Pe Culmi</b> Împărtășește Traseele Tale Montane
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            dolores consequatur sunt, maiores quibusdam vero hic porro dolorem
+            ab unde. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Architecto dolores consequatur sunt, maiores quibusdam vero hic
+            porro dolorem ab unde.
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
