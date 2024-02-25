46b3c647706f49a2c32d1d00fccd3133c25a9fc6
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-02-25 16:13:00 +0200

    Category Component Design.

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import React from "react";
 import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
   return (
-    <div className={styles.container}>CategoryList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Categorii Populare</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=travel"
+          className={`${styles.category} ${styles.travel}`}
+        >
+          <Image
+            src="/travel.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Travel
+        </Link>
+        <Link
+          href="/blog?cat=culture"
+          className={`${styles.category} ${styles.culture}`}
+        >
+          <Image
+            src="/culture.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Culture
+        </Link>
+        <Link
+          href="/blog?cat=coding"
+          className={`${styles.category} ${styles.coding}`}
+        >
+          <Image
+            src="/coding.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Coding
+        </Link>
+        <Link
+          href="/blog?cat=food"
+          className={`${styles.category} ${styles.food}`}
+        >
+          <Image
+            src="/food.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          Food
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default CategoryList
+export default CategoryList;
