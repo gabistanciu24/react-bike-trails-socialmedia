ec5e3634651b1f15fd9d25703b15a1729ab1b1d6
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-02-27 13:03:57 +0200

    Side Menu Component Design.

--- src/app/components/menu/Menu.jsx
+++ src/app/components/menu/Menu.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
-    <div className={styles.container}>Menu</div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>Ceva nou</h2>
+      <h1 className={styles.title}>Trasee Populare</h1>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Exploreaza tipurile</h2>
+      <h1 className={styles.title}>Categorii</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Recomandari</h2>
+      <h1 className={styles.title}>Pe unde s-o apuc?</h1>
+      <MenuPosts withImage={true} />
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
