2cd50a4c5b09d5d29595b3daa79c733126a77b11
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-02-27 13:10:44 +0200

    Pagination Button Design.

--- src/app/components/pagination/Pagination.jsx
+++ src/app/components/pagination/Pagination.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
   return (
-    <div className={styles.container}>Pagination</div>
-  )
-}
+    <div className={styles.container}>
+      <button className={styles.button}>Inapoi</button>
+      <button className={styles.button}>Inainte</button>
+    </div>
+  );
+};
 
-export default Pagination
+export default Pagination;
