f159b9adae01516d2d028b8a8f24ae7354290787
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-02-28 23:11:05 +0200

    BikeTrails Prisma Schema.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,17 @@ import Menu from "./components/menu/Menu";
 import styles from "./homepage.module.css";
 import Link from "next/link";
 
-export default function Home() {
-  return <div className={styles.container}>
-    <Featured/>
-    <CategoryList/>
-    <div className={styles.content}>
-      <CardList/>
-      <Menu/>
+export default function Home(searchParams) {
+  const page = parseInt(searchParams.page) || 1;
+
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={page} />
+        <Menu />
+      </div>
     </div>
-  </div>;
+  );
 }
